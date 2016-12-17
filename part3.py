f772239b8ef5b345222028e151b30a33866bc3a0
kcunanan <kevin.a.cunanan@gmail.com>
2016-12-16 17:10:43 -0800

    added/removed some comments and finalized paper

--- part3.py
+++ part3.py
@@ -17,7 +17,7 @@ def parseLine(line):
 		return (name, category, reviews) # name(string), categories (list), review (string)
 
 
-conf = SparkConf().setAppName("YELP").set("spark.executor.memory", "4g")
+conf = SparkConf().setAppName("YELP").set("spark.executor.memory", "7g")
 sc = SparkContext(conf=conf)
 
 # Load documents (one per line).
@@ -46,18 +46,11 @@ model = LogisticRegressionWithLBFGS.train(training, numClasses=len(labelSet))
 
 # test a few items
 labelsAndPreds = test.map(lambda p: (labelSet[int(p.label)], p.label,model.predict(p.features) ))
-print labelsAndPreds.take(100)
+temp = labelsAndPreds.take(50)
 
 for index in range(len(temp)):
 	realVal = int(temp[0][1]) # typecasting real value from float to int so we can compare to predictedVal
 	predictedVal = int(temp[0][2])
-
-	# if (realVal == predictedVal):
-	# 	#output = ([x[1] for x in labelsAndPreds],true)
-	# 	print "true"
-	# else:
-	# 	#output = ([x[1] for x in labelsAndPreds],false)
-	# 	print "false"
 
 	accurate = 0
 	inaccurate = 0
